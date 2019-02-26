e274ef53c572f077510621bf3c01265cf3e78c6d
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-26 13:07:30 -0300

    getElementById JavaScript

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // console.log(document.title = 'JavaScript DOM');
 
+/* ********************** document.write ************************** */
 // document.write('Sou o uso do document.write');
 // document.write('<h1>Sou o uso do document.write</h1>');
 // document.write('<p>Sou um parágrafo</p>');
+
+
+
+/* ********************** getElementById ************************** */
+// document.getElementById('test');
+// console.log(document.getElementById('test'));
+
+// document.getElementById('test').innerHTML = 'Sou um novo texto';
+// setTimeout(function () {
+    // document.getElementById('test').innerHTML = 'Sou um novo texto';
+    // document.getElementById('test').innerText = 'Sou um novo texto';
+
+    // document.getElementById('test-id').innerHTML = '<h3>Sou um P JS</h3>';
+    // document.getElementById('test-id').innerText = '<h3>Sou um P JS</h3>';
+// }, 3000); // MUDA O CONTEÚDO DA PÁGINA DEPOIS DE 3 SEGUNDOS
