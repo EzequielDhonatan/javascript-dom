152ee5d4b3c3ab8a1c1daaf46ba9c7114e44695b
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-26 12:43:37 -0300

    document.write JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log(document.title = 'JavaScript DOM');
+// console.log(document.title = 'JavaScript DOM');
+
+// document.write('Sou o uso do document.write');
+// document.write('<h1>Sou o uso do document.write</h1>');
+// document.write('<p>Sou um parágrafo</p>');
