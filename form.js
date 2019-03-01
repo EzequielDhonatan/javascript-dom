5ba1b6041b7ad3a336978fc96336a6cc66f0406b
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-28 21:21:24 -0300

    Capturar Valores do Formulário com JavaScript (checkbox, select, radio e textarea)

--- form.js
+++ form.js
@@ -1,9 +1,24 @@
 function onSubmit (event) {
     var name = document.getElementsByName('name')[0].value;
     document.getElementsByClassName('result')[0].innerText = 'Olá ' + name;
-    
+
     console.log(document.getElementsByName('email')[0].value);
     console.log(document.getElementsByName('password')[0].value);
+
+    // console.log(document.getElementsByName('terms')[0].checked);
+    console.log(document.getElementById('terms').checked);
+
+    var state = document.getElementsByName('state')[0].value;
+    if (state == '') {
+        alert('Selecione o estado!');
+    }
+
+    var sexo = document.getElementsByName('sexo');
+    console.log(sexo[0].checked);
+    console.log(sexo[1].checked);
+    console.log(document.querySelector('input[name="sexo"]:checked').value);
+
+    console.log(document.getElementsByClassName('desc')[0].value);
 
     return false;
 }
