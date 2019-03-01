275866ff724cd63ae07da4d225fbe25b2f0f96cd
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-28 22:20:49 -0300

    Cancelar Propagação Padrão de Eventos com JavaScript

--- events.js
+++ events.js
@@ -1,5 +1,20 @@
-document.addEventListener('click', eventClick);
+// document.addEventListener('click', eventClick);
 
+/*
 function eventClick () {
     alert('Clicou');
+}
+ */
+
+function changeColor (event, color) {
+    document.body.style.background = color;
+
+    event = event || window.event // IE
+
+    console.log(event);
+
+    if (event.preventDefault) event.preventDefault();
+    if (event.returnValue) event.returnValue();
+
+    return false;
 }
