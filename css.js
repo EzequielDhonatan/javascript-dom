fa73ccf7a53458af500fdf7f2f8ab624705a160c
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-28 21:36:26 -0300

    Alterar CSS da Página com JavaScript

---
+++ css.js
@@ -0,0 +1,13 @@
+var body = document.body
+// body.style.backgroundColor = '#000';
+// body.style.color = '#FFF';
+
+setTimeout(function () {
+    body.style.backgroundColor = '#000';
+    body.style.color = '#FFF';
+}, 2000);
+
+var title = document.getElementById('title');
+title.style.border = "2px solid #999";
+title.style.padding = "10px";
+
