17d1cc9f948d424a7f81b77b1a2ec051dcd6df8b
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-28 21:43:50 -0300

    Mudar a Cor da Página Dinâmicamente com JavaScript

--- css.js
+++ css.js
@@ -3,11 +3,16 @@ var body = document.body
 // body.style.color = '#FFF';
 
 setTimeout(function () {
-    body.style.backgroundColor = '#000';
-    body.style.color = '#FFF';
+    // body.style.backgroundColor = '#000';
+    // body.style.color = '#FFF';
 }, 2000);
 
 var title = document.getElementById('title');
 title.style.border = "2px solid #999";
 title.style.padding = "10px";
 
+
+function setBackground (color) {
+    body.style.color = '#FFF';
+    body.style.backgroundColor = color;
+};
